635572366b358af23187251db9ae839f726da540
Leonardo Peripolli <102085692+leopp18@users.noreply.github.com>
2022-10-11 14:35:45 -0300

    07.01

---
+++ Ex01.cs
@@ -0,0 +1,21 @@
+﻿namespace Lista07_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, author;
+            int year;
+
+            Console.WriteLine("Insira o nome do livro:");
+            name = Console.ReadLine();
+            Console.WriteLine("Insira o nome do autor:");
+            author = Console.ReadLine();
+            Console.WriteLine("Insira o ano de publicação:");
+            year = int.Parse(Console.ReadLine());
+
+            Livro l1 = new Livro(name, author, year);
+            l1.exibirDados();
+        }
+    }
+}
---
+++ Livro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista07_Ex01
+{
+    internal class Livro
+    {
+        private string nome;
+        private string autor;
+        private int ano;
+
+        public Livro(string nome, string autor, int ano)
+        {
+            this.nome = nome;
+            this.autor = autor;
+            this.ano = ano;
+        }
+
+        public void exibirDados()
+        {
+            Console.WriteLine("Título: " + nome);
+            Console.WriteLine("Autor: " + autor);
+            Console.WriteLine("Ano: " + ano);
+        }
+    }
+}
